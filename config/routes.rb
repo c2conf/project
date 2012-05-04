fe47c2c221bfc8dc12ebf0855dca2d7f83d4b6d7
Preeti <dev1@c2conf.com>
2012-05-04 22:35:53 +0530

    Changed app for initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,10 @@ Project::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => 'home#index'
+	group :production do
+		gem 'pg'
+	end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
