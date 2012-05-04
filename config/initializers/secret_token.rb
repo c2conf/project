18c21a4dc4011db7f871f8bbe83357185a35c674
Preeti <dev1@c2conf.com>
2012-05-04 21:52:44 +0530

    Initial repo push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '206e8a99140e730561cc608b64711f0994bac1c2e07b5c3de200a0ebae83a4c0136b3776815f872d3a3752992a7e3ce72085eb40c3ef9a3c74f1407e8ec8dc05'
