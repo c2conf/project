0cbacff3cbd3ab9064581d7c712e5b559c9aa9e7
Preeti <dev1@c2conf.com>
2012-05-04 22:39:26 +0530

    Added PG gem

--- config/routes.rb
+++ config/routes.rb
@@ -49,9 +49,6 @@ Project::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
-	group :production do
-		gem 'pg'
-	end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
