abdf2a1b2441c6f8de7b783ee510c48261658e28
Preeti <dev1@c2conf.com>
2012-05-06 21:15:40 +0530

    Changed production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,9 +5,9 @@ Project::Application.configure do
 		:address        => 'smtp.sendgrid.net',
 		:port           => '587',
 		:authentication => :plain,
-		:user_name      => "app4462770@heroku.com",
-		:password       => "z9bmr1ws",
-		:domain         => 'www.c2conf.com'
+		:user_name      => ENV["SENDGRID_USERNAME"],
+		:password       => ENV["SENDGRID_PASSWORD"],
+		:domain         => 'heroku.com'
 	}
 	ActionMailer::Base.delivery_method = :smtp
 
